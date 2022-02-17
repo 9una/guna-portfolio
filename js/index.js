1b7c8cb0dabdb8abd45d643af8ca8bcd8777fa43
guna_9 <guna_9@naver.com>
2022-02-17 23:05:00 +0900

    src 수정

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    $('#about').load("../about.html");
-    $('#portfolio').load("../portfolio.html");
+    $('#about').load("about.html");
+    $('#portfolio').load("portfolio.html");
 });
 
 
